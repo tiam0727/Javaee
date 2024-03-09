0b766e16e2cb5b2a5a2b2db954e4dcfd3cafc3fb
xj <2579211260@qq.com>
2024-03-09 22:34:49 +0800

    添加集合框架图内容

---
+++ src/com/jj/collection_/Collection01.java
@@ -0,0 +1,23 @@
+package com.jj.collection_;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * @author: xj
+ * @description: 集合框架了解
+ * @dateTime: 2024/3/9 22:21
+ **/
+public class Collection01 {
+    public static void main(String[] args) {
+        //Collection
+        //Map
+        /**
+         * 1. 集合主要分为两组（单列集合，双列集合）
+         *      单列集合存放一般为单个对象
+         *      双列集合存放的一般为键值对
+         *      Collection接口有两个重要的子接口 List Set，他们的实现子类都是单列集合
+         *      Map接口的实现子类是双列结合
+         */
+    }
+}
